d2e23a4c411f50e8ab7c066b1c76c20b67c8f9a0
Thomas Sprock <bettercallthomas@outlook.com>
2024-01-18 11:52:02 +0100

    improved error handling

--- ubuntu-mirrors/ubuntu-mirrors.py
+++ ubuntu-mirrors/ubuntu-mirrors.py
@@ -5,22 +5,32 @@ from urllib.parse import urlparse
 
 url = 'https://launchpad.net/ubuntu/+archivemirrors'
 
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-tables = soup.find_all('table', attrs={'id': 'mirrors_list'})
+try:
+    response = requests.get(url)
+    response.raise_for_status()
+except (requests.exceptions.RequestException, requests.exceptions.HTTPError) as err:
+    print(f"An error occurred while trying to fetch data: {err}")
+else:
+    try:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        tables = soup.find_all('table', attrs={'id': 'mirrors_list'})
 
-domains = []
+        domains = []
 
-for table in tables:
-  links = table.find_all('a')
-  for link in links:
-    href = link.get('href')
-    if href:
-      domain = urlparse(href).netloc
-      if domain:
-        domains.append(domain)
+        for table in tables:
+            links = table.find_all('a')
+            for link in links:
+                href = link.get('href')
+                if href:
+                    domain = urlparse(href).netloc
+                    if domain:
+                        domains.append(domain)
 
-unique_domains = list(set(domains))
-with open('ubuntu-mirrors/ubuntu-mirrors.txt', 'w') as file:
-    for domain in unique_domains:
-        file.write(domain + '/\n')
+        unique_domains = list(set(domains))
+
+        if unique_domains:
+            with open('ubuntu-mirrors/ubuntu-mirrors.txt', 'w') as file:
+                for domain in unique_domains:
+                    file.write(domain + '/\n')
+    except Exception as err:
+        print(f"An error occurred while parsing the data: {err}")
