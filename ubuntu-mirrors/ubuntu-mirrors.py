12b9580e3c27a6f8d684eec2e52d6bc5ffb9447e
Thomas Sprock <bettercallthomas@outlook.com>
2024-01-18 09:29:04 +0100

    Update ubuntu-mirrors.py

--- ubuntu-mirrors/ubuntu-mirrors.py
+++ ubuntu-mirrors/ubuntu-mirrors.py
@@ -6,28 +6,20 @@ from urllib.parse import urlparse
 url = 'https://launchpad.net/ubuntu/+archivemirrors'
 
 response = requests.get(url)
-
 soup = BeautifulSoup(response.text, 'html.parser')
-
-# Find all <table> elements
 tables = soup.find_all('table', attrs={'id': 'mirrors_list'})
 
-# Initialize a list to store the domains
 domains = []
 
-# Iterate over each <table>
 for table in tables:
-    # Find all <a> tags within the table
     links = table.find_all('a')
-    # Extract the href attribute and parse the domain
     for link in links:
         href = link.get('href')
         if href:
             domain = urlparse(href).netloc
             domains.append(domain)
 
-# Remove duplicates and print the list of domains
 unique_domains = list(set(domains))
-with open('ubuntu-mirrors.txt', 'w') as file:
+with open('ubuntu-mirrors/ubuntu-mirrors.txt', 'w') as file:
     for domain in unique_domains:
         file.write(domain + '/\n')
