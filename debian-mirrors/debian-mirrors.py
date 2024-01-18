0dc6451cfe6e07ed41a06636f92eeeafb53bd4a6
Thomas Sprock <bettercallthomas@outlook.com>
2024-01-18 11:50:45 +0100

    improved error handling

--- debian-mirrors/debian-mirrors.py
+++ debian-mirrors/debian-mirrors.py
@@ -5,22 +5,32 @@ from urllib.parse import urlparse
 
 url = 'https://www.debian.org/mirror/list'
 
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-tables = soup.find_all('table')
+try:
+    response = requests.get(url)
+    response.raise_for_status()
+except (requests.exceptions.RequestException, requests.exceptions.HTTPError) as err:
+    print(f"An error occurred while trying to fetch data: {err}")
+else:
+    try:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        tables = soup.find_all('table')
 
-domains = []
+        domains = []
 
-for table in tables:
-  links = table.find_all('a')
-  for link in links:
-    href = link.get('href')
-    if href:
-      domain = urlparse(href).netloc
-      if domain:
-        domains.append(domain)
-                
-unique_domains = list(set(domains))
-with open('debian-mirrors/debian-mirrors.txt', 'w') as file:
-  for domain in unique_domains:
-    file.write(domain + '/\n')
+        for table in tables:
+            links = table.find_all('a')
+            for link in links:
+                href = link.get('href')
+                if href:
+                    domain = urlparse(href).netloc
+                    if domain:
+                        domains.append(domain)
+
+        unique_domains = list(set(domains))
+
+        if unique_domains:
+            with open('debian-mirrors/debian-mirrors.txt', 'w') as file:
+                for domain in unique_domains:
+                    file.write(domain + '/\n')
+    except Exception as err:
+        print(f"An error occurred while parsing the data: {err}")
