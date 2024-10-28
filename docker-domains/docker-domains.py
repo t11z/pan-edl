bbacbe7260592711c1155ba0cdfd0d808b2a2a05
Thomas Sprock <bettercallthomas@outlook.com>
2024-10-28 09:39:25 +0100

    change to align with new docs article structure

--- docker-domains/docker-domains.py
+++ docker-domains/docker-domains.py
@@ -14,26 +14,27 @@ else:
     try:
         soup = BeautifulSoup(response.text, 'html.parser')
         h2 = soup.find('h2', attrs={'id': 'domain-urls-to-allow'})
-        table = h2.find_next_sibling('table')
+        div = h2.find_next('div', class_='overflow-x-auto')
 
         domains = []
         domains.append("download.docker.com")
 
-        if table:
-            links = table.find_all('a')
-            for link in links:
-                href = link.get('href')
-                if href:
-                    domain = urlparse(href).netloc
-                    if domain:
+        if div:
+            table = div.find('table')
+            if table:
+                links = table.find_all('a')
+                for link in links:
+                    href = link.get('href')
+                    if href:
+                        domain = urlparse(href).netloc
                         domains.append(domain)
-
+                
         unique_domains = list(set(domains))
         unique_domains.sort()
-
+ 
         if unique_domains:
             with open('docker-domains/docker-domains.txt', 'w') as file:
                 for domain in unique_domains:
                     file.write(domain + '/\n')
-    except Exception as err:
-        print(f"An error occurred while parsing the data: {err}")
+    except AttributeError as e:
+        print(f"An error occurred while parsing the HTML: {e}")
